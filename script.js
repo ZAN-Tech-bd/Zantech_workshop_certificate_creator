e6566be4480dd419daccfefa6e26d58b67ea0f73
Nakib00 <2022142@iub.edu.bd>
2025-10-02 22:48:41 +0600

    vzcx

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// This file is ready for future enhancements.
+// For example, you could dynamically load participant data
+// and generate multiple certificates.
+
+console.log("Certificate page loaded successfully.");
